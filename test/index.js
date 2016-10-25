3e71703b14eef9a29fdce1e6eb87a97a3458a751
Elliott Foster <elliottf@codebrews.com>
2016-10-25 17:42:06 -0500

    chore(semantic-release): Use semantic release
    BREAKING CHANGE: The code has been refactord to take advantage of
    es6 features only available in node6 or higher.

--- test/index.js
+++ test/index.js
@@ -1,70 +1,76 @@
-var spawn = require('child_process').spawn;
-var Validator = require('../').Validator;
+'use strict';
+
+const Validator = require('../').Validator;
 
 module.exports = {
-  setUp: function (cb) {
+  setUp(cb) {
     this.validator = new Validator();
     cb();
   },
   validate: {
-    valid: function (test) {
+    valid(test) {
       test.expect(1);
 
-      test.doesNotThrow(function () {
-        this.validator.validate({meta: {foo: 'bar'}});
-      }.bind(this));
+      test.doesNotThrow(() => {
+        this.validator.validate({ meta: { foo: 'bar' } });
+      });
 
       test.done();
     },
-    invalid: function (test) {
+    invalid(test) {
       test.expect(1);
 
-      test.throws(function () {
+      test.throws(() => {
         this.validator.validate({});
-      }.bind(this));
+      });
 
       test.done();
     },
-    listErrors: function (test) {
+    listErrors(test) {
       test.expect(1);
 
       try {
         this.validator.validate({});
-      } catch (e) {
-        test.equal(e.errors[0].message, 'should have required property \'data\'', 'Unexpected error reported.');
+      }
+      catch (e) {
+        test.equal(
+          e.errors[0].message,
+          'should have required property \'data\'',
+          'Unexpected error reported.'
+        );
       }
 
       test.done();
-    }
+    },
   },
   isValid: {
-    valid: function (test) {
+    valid(test) {
       test.expect(1);
-      test.ok(this.validator.isValid({meta: {foo: 'bar'}}), 'Valid JSON API not found.');
+      test.ok(this.validator.isValid({ meta: { foo: 'bar' } }), 'Valid JSON API not found.');
       test.done();
     },
-    invalid: function (test) {
+    invalid(test) {
       test.expect(1);
       test.ok(!this.validator.isValid({}), 'Invalid JSON API not found.');
       test.done();
-    }
+    },
   },
-  customSchema: function (test) {
+  customSchema(test) {
     test.expect(1);
 
     // NOTE: not valid JSON API, but a valid JSON schema.
-    var validator = new Validator({
+    const validator = new Validator({
       type: 'object',
       required: ['hello'],
       properties: {
         hello: {
-          type: 'string'
-        }
-      }
+          type: 'string',
+        },
+      },
     });
 
-    test.ok(validator.isValid({hello: 'world'}));
+    test.ok(validator.isValid({ hello: 'world' }));
     test.done();
-  }
+  },
 };
 
